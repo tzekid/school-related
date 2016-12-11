4ffb1967c89f8a51fcb8dfa6ac89b1bd73071556
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2016-12-11 02:19:48 +0100

    commit

--- Q1/04_Listz/Program.cs
+++ Q1/04_Listz/Program.cs
@@ -1,122 +1,231 @@
 using System;
-using System.Collections.Generic;
-using DataTypes;
-
-/*** Listz *** !!!
-/** Teh code was done 
- * in a rush in ~1.5h.
- * Most of the code looks functional.
- * The looks can be deceiving.
- */
 
 namespace ConsoleApplication {
-    public class Interface {
-        char[] symbols = {'λ', '>'};
+    public class Program {
 
-        public void start() {
-            DataTypes.Types Types = new DataTypes.Types();
+        public class Node {
+            public string value = null;
+            public Node nextElement = null;
+        }
 
-            Dictionary<string, Types.LinkedList> listz = new Dictionary<string, Types.LinkedList>();
-            Dictionary<string, Types.Node> nodez = new Dictionary<string, Types.Node>();
+        public class LinkedList {
+            public Node head = new Node();
+            public Node body = new Node();
+            public bool debg = true;
 
-            Console.WriteLine("Welcum ot Listz!\n");
-
-            while (true) {
-                Console.Write(symbols[0] + " ");
-                string input = Console.ReadLine();
-                string[] inputs = input.Split(' ');
-
-                if (input == "help") {
-                    Console.WriteLine("Nothing here.");
-                } else if (input == "quit" || input == "exit") break;
-
-                else if (inputs[0] == "create") {
-                    try {
-                        if (inputs[1] == "Node") {
-                            if (inputs[2] != "") {
-                                nodez.Add(inputs[2], new Types.Node());
-                                nodez[inputs[2]].value = inputs[2];
-                                Console.WriteLine("Teh node `"+ inputs[2] +"` was created.");
-                            } else Console.WriteLine("usage: create {Node / List} {name}");
-                        } else if (inputs[1] == "List") {
-                            if (inputs[2] != "") {
-                                listz.Add(inputs[2], new Types.LinkedList());
-                                Console.WriteLine("Teh list `"+ inputs[2] +"` was created.");
-                            } else Console.WriteLine("usage: create {Node / List} {name}");
-                        } else Console.WriteLine("usage: create {Node / List} {name}");
-                    } catch { Console.WriteLine("usage: create {Node / List} {name}"); }
+            // Unsorted add
+            public void add_ (Node node) {
+                if (this.head.value == null)
+                    this.head = node;
+                else if (this.head.value != null && this.body.value == null) {
+                    this.body = node;
+                    this.head.nextElement = node;
+                } else {
+                    this.body.nextElement = node;
+                    this.body = node;
                 }
+                if (this.debg) Console.WriteLine(this);
+            }
 
-                else if (inputs[0] == "assign") {
-                    foreach (var node in nodez) {
-                        if (inputs[1] == node.Key) {
-                            try { node.Value.value = inputs[2];
-                                  Console.WriteLine("`"+ node.Key +"` now has the value `"+ inputs[2] +"`"); }
-                            catch { Console.WriteLine("usage: assign {node} {value}"); }
-                        } else Console.WriteLine("usage: assign {node} {value}");
-                    }
-                }
-
-                else if (inputs[0] == "add") {
-                    if (inputs[1] == "all") {
-                        if (listz.ContainsKey(inputs[2]))
-                            foreach (var node in nodez) listz[inputs[2]]._add(nodez[node.Key]);
-                        else Console.WriteLine("usage: add all {list}");
+            // recursive sorted add
+            public void _add(Node node) {
+                if (this.head.value == null) {
+                    this.head = node;
+                } else if (this.body.value == null) {
+                    if (this.head.value.CompareTo(this.body.value) != 1) {
+                        this.body = node;
+                        this.head.nextElement = this.body;
                     } else {
-                    try {
-                        if (nodez.ContainsKey(inputs[1])) {
-                            if (inputs[2] == "to") {
-                                if (listz.ContainsKey(inputs[3]))
-                                    listz[inputs[3]].add(nodez[inputs[1]]);
-                                else { Console.WriteLine("usage: add {Node} to {List}"); }
-                            } else Console.WriteLine("usage: add {Node} to {List}");
-                        } else { Console.WriteLine("usage: add {Node} to {List}"); }
-                    } catch { Console.WriteLine("usage: add {Node} to {List}"); } }
-                }
+                        this.body = this.head;
+                        this.head = node;
+                        this.head.nextElement = this.body;
+                    }
+                } else {
+                    Node prevNode = this.head;
+                    Node currNode = this.head.nextElement;
+                    Node nextNode = currNode.nextElement;
 
-                else if (inputs[0] == "remove") {
-                    try {
-                        if (nodez.ContainsKey(inputs[1])) {
-                            if (inputs[2] == "from") {
-                                if (listz.ContainsKey(inputs[3]))
-                                    listz[inputs[3]].remove(nodez[inputs[1]]);
-                                else Console.WriteLine("usage: remove Node from List");
-                            } else Console.WriteLine("usage: remove Node from List");
-                        } else Console.WriteLine("usage: remove Node from List");
-                    } catch { Console.WriteLine("usage: remove Node from List"); }
-                }
+                    while(true) {
+                        if (node.value.CompareTo(currNode.value) != 1) {
+                            prevNode.nextElement = node;
+                            node.nextElement = nextNode;
+                            this._add(currNode);
+                        } else {
+                            prevNode = currNode;
+                            currNode = nextNode;
+                            nextNode = nextNode.nextElement;
+                        }
 
-                foreach (var node in nodez) {
-                    if (inputs[0] == node.Key) Console.WriteLine("Node `"+ node.Key +"` holding the value `"+ node.Value +"`");
-                }
-
-                foreach (var list in listz) {
-                    if (inputs[0] == list.Key && inputs.Length == 1) Console.WriteLine(listz[list.Key]);
-                    else if (inputs[0] == list.Key && inputs.Length != 1) {
-                        try {
-                            if (inputs[1] == "changeAtPos") {
-                                if (Convert.ToInt32(inputs[2]) > 0) {
-                                    if (inputs[3] == "with") {
-                                        if (nodez.ContainsKey(inputs[4]))
-                                            listz[list.Key].changeAtPos(Convert.ToInt32(inputs[1]), nodez[inputs[4]]);
-                                        else Console.WriteLine(listz[list.Key] + "\nusage: {list} (`changeAtPos`)");
-                                    } else Console.WriteLine(listz[list.Key] + "\nusage: {list} (`changeAtPos`)");
-                                } else Console.WriteLine(listz[list.Key] + "\nusage: {list} (`changeAtPos`)");
-                            } // else Console.WriteLine(listz[list.Key] + "\nusage: {list} (`changeAtPos`)");
-                        } catch { Console.WriteLine(listz[list.Key] + "\nusage: {list} (`changeAtPos`)"); }
+                        if (nextNode == null) break;
                     }
                 }
             }
-        }
-    }
 
-    public class Program {
-        Types Types = new DataTypes.Types();
+
+            public void add (Node node) {
+                Node prevNode = new Node();
+                Node currNode = new Node();
+                Node nextNode = new Node();
+
+                if (this.head.value == null) {
+                    this.head = node;
+                } else if (this.body.value == null) {
+                    if (this.head.value.CompareTo(this.body.value) != 1) {
+                        this.body = node;
+                        this.head.nextElement = this.body;
+                    } else {
+                        this.body = this.head;
+                        this.head = node;
+                        this.head.nextElement = this.body;
+                    }
+                } else {
+                     if (this.head.value.CompareTo(node.value) == 1) {
+                        currNode = this.head;
+                        nextNode = this.body;
+
+                        this.head = node;
+                        this.body = currNode;
+
+                        this.head.nextElement = currNode;
+                        this.body.nextElement = nextNode;
+                    //} else if (this.head.value.CompareTo(node.value) != 1) {
+                    } else if (this.head.value.CompareTo(node.value) == 0) {
+                        prevNode = this.head;
+                        nextNode = this.head.nextElement;
+
+                        this.head.nextElement = node;
+                        node.nextElement = nextNode;
+                    } else {
+                        prevNode = this.head;
+                        currNode = this.head.nextElement;
+
+                        while(currNode.value != null) {
+                            if (currNode.value.CompareTo(node.value) == 1) {
+                                prevNode.nextElement = node;
+                                node.nextElement = currNode;
+
+                                if (currNode.nextElement == null)
+                                    this.body = node;
+
+                                break;
+                            } else if (currNode.nextElement == null) {
+                                currNode.nextElement = node;
+                                this.body = node;
+                                break;
+                            } else {
+                                prevNode = currNode;
+                                currNode = currNode.nextElement;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine(this);
+            }
+
+            public int count () {
+                int c = 1;
+                Node tempNode = this.head;
+
+                while(tempNode.nextElement != null) {
+                    c++;
+                    tempNode = tempNode.nextElement;
+                }
+
+                return c;
+            }
+
+            public Node find (string value) {
+                Node tempNode = this.head;
+
+                while (tempNode.nextElement != null) {
+                    if (tempNode.value == value)
+                        return tempNode;
+                }
+
+                Console.WriteLine("There is no object w/ the value: '"+ value +"' in this list.");
+                return null;
+            }
+
+            public int findNodePos(Node node) {
+                Node tempNode = this.head;
+                int cunter = 0;
+
+                while (tempNode != node) {
+                    cunter ++;
+
+                    tempNode = tempNode.nextElement;
+                    if (tempNode == null) return -1;
+                }
+
+                return cunter;
+            }
+
+            public Node returnNodeAtPos(int pos) {
+                Node tempNode = this.head;
+
+                if (pos == 0) return tempNode;
+                else if (pos >= this.count()) return null;
+                else {
+                    for (int i = 1; i <= pos; i++) {
+                        tempNode = tempNode.nextElement;
+                    }
+                    return tempNode;
+                }
+            }
+
+            public void changeAtPos (int pos, Node node) {
+                Node tempNode = this.head;
+                Node tempNextNode = new Node();
+                Node tempNextNextNode = new Node();
+
+                int cunt = this.count() - 1;
+                while (cunt < this.count() - 1) {
+                    tempNode = tempNode.nextElement;
+                    tempNextNode = tempNode.nextElement;
+                }
+
+                tempNextNextNode = tempNextNode.nextElement;
+
+                if (node == null) {
+                    tempNode.nextElement = tempNextNextNode;
+                } else if (cunt == 0) {
+                    node.nextElement = this.head.nextElement;
+                    this.head = node;
+                } else {
+                    tempNode.nextElement = node;
+                    tempNextNode = node;
+                    tempNextNode.nextElement = tempNextNextNode;
+                }
+            }
+
+            // rewrite remove :))
+            public void remove(Node node) {
+                Node oldNode = this.head;
+                Node tempNode = new Node();
+
+                int pos = findNodePos(node);
+
+                changeAtPos(pos, null);
+            }
+
+            public override string ToString() {
+                Node tempNode = this.head;
+                string tempString = this.GetType().Name.ToString() + ": [";
+                while (tempNode != null) {
+                    if (tempNode.nextElement != null) {
+                        tempString += " \"" + tempNode.value + "\", ";
+                    } else {
+                        tempString += " \"" + tempNode.value + "\", null ]";
+                    }
+                    tempNode = tempNode.nextElement;
+                }
+
+                return tempString;
+            }
+        }
 
         public static void Main(string[] args) {
-            Interface cli = new Interface();
-
-            cli.start();
         }
     }
 }
