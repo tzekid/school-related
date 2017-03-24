b7ff650f15c3e1b5f8482b9c6f1b0a75faf04ca0
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2017-03-24 08:35:24 +0100

    Node Balance is doen. Level-wise yielding is done. Recursion is hard.

--- Q2/05_Treez/treez.cr
+++ Q2/05_Treez/treez.cr
@@ -5,9 +5,9 @@ module Naive
   dick.add 2
   dick.add 3
   dick.add 1
-  dick.add 6
   dick.add 5
   dick.add 7
+  dick.add 6
 
   # p "preOrder"
   # dick.preOrder
@@ -17,7 +17,13 @@ module Naive
   # p "postOrder"
   # dick.postOrder()
 
+  p "Nodes printed level-wise"
   dick.breadthFirst{ |x| p x.value }
+
+  puts # Newline
+
+  p "Balance of each individual node"
+  dick.breadthFirst{ |x| p dick.nodeBalance x }
 
   # p dick.nodeBalance 
 end
